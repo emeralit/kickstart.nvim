1b8e655949f3b43c905938581f130d5a59aa5cef
Domenico Ipri <domipri@icloud.com>
2023-05-04 15:23:47 +0200

    Carico gli snippets nel modulo luasnip.lua

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -5,8 +5,11 @@ return {
 			-- follow latest release.
 			version = "1.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 			-- install jsregexp (optional!).
-			build = "make install_jsregexp"
+			build = "make install_jsregexp",
+			dependencies = { "rafamadriz/friendly-snippets" },
+			config = function()
+				require('luasnip.loaders.from_vscode').lazy_load()
+			end,
 		}
 	},
-	"rafamadriz/friendly-snippets",
 }
