8a018d0a8ba475d0dceb094e1ee0f5d0135d809a
Domenico Ipri <domipri@icloud.com>
2023-10-16 15:15:31 +0200

    modified files blankline.lua catppuccin.lua

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+--
+-- Functional wrapper for mapping custom keybindings
+
+return {
+	{
+		-- Theme
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme 'catppuccin'
+		end,
+	},
+}
